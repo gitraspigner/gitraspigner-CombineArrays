7496d0fcd3133044b5c47dff8f3f0198737e5b9a
Ravi Spigner <585pieces@protonmail.com>
2024-02-15 03:25:40 +0000

    completed Coding Challenge: Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] result = new int[arr1.length + arr2.length];
+        int resultI = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            result[resultI] = arr1[i];
+            resultI++;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            result[resultI] = arr2[i];
+            resultI++;
+        }
+        return result;
     }
 }
